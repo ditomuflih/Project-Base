71bf99c1798b1fcb43d680f00a9aa9e26a745348
ditomuflih <110583633+ditomuflih@users.noreply.github.com>
2023-12-12 21:29:12 +0700

    projectbase.py

--- projectbase.py
+++ projectbase.py
@@ -1,139 +1,116 @@
 import heapq
-from datetime import datetime, timedelta
+import random
 
-# Data kelas
-matakuliah_data = {
-    "Pancasila": [
-        "Senin 08:00 - 10:00", "Selasa 10:00 - 12:00",
-        "Rabu 14:00 - 16:00", "Kamis 08:00 - 10:00",
-        "Jumat 10:00 - 12:00", "Sabtu 08:00 - 10:00"
+class ScheduleNode:
+    def __init__(self, day, class_name, start_time, end_time):
+        self.day = day
+        self.class_name = class_name
+        self.start_time = start_time
+        self.end_time = end_time
+
+    def __lt__(self, other):
+        # Comparison for the priority queue
+        return self.end_time < other.end_time
+    
+def is_valid_schedule(schedule, current):
+    # Check if the current class can be added to the schedule without overlapping
+    overlapping_classes = [c for c in schedule[current.day] if not (current.end_time <= c.start_time or current.start_time >= c.end_time)]
+    return not overlapping_classes
+
+def find_schedule(classes):
+    days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat']
+    schedule = {day: [] for day in days}
+    heap = []
+
+    selected_classes = set()
+
+    for cls in classes:
+        while True:
+            random_class = random.choice(classes[cls])
+            class_name = cls + ' ' + random_class[0]
+
+            # Ensure each class is selected only once
+            if class_name not in selected_classes:
+                selected_classes.add(class_name)
+                break
+
+        heapq.heappush(heap, ScheduleNode(random_class[1], class_name, random_class[2], random_class[3]))
+
+    while heap:
+        current = heapq.heappop(heap)
+        if is_valid_schedule(schedule, current):
+            schedule[current.day].append(current)
+
+    return schedule
+
+def print_schedule(schedule):
+    for day in schedule:
+        print(day)
+        if not schedule[day]:
+            print("Hari ini kosong.")
+        else:
+            for cls in schedule[day]:
+                print(f"{cls.class_name} Jam {cls.start_time}-{cls.end_time}")
+        print()
+
+# Jadwal untuk setiap mata kuliah
+classes = {
+    'Jaringan Komputer': [
+        ('A', 'Senin', '15:30', '17:09'),
+        ('B', 'Senin', '16:20', '18:09'),
+        ('C', 'Senin', '10:20', '11:59'),
+        ('D', 'Rabu', '16:20', '18:09'),
+        ('E', 'Rabu', '15:30', '17:09'),
+        ('F', 'Rabu', '10:20', '11:59'),
+        ('G', 'Selasa', '09:30', '11:09'),
     ],
-    "Kecerdasan Buatan": [
-        "Senin 14:00 - 16:00", "Selasa 08:00 - 10:00",
-        "Rabu 10:00 - 12:00", "Kamis 14:00 - 16:00",
-        "Jumat 08:00 - 10:00", "Sabtu 10:00 - 12:00"
+    'Basis Data': [
+        ('A', 'Senin', '9:30', '11:09'),
+        ('B', 'Selasa', '15:30', '17:09'),
+        ('C', 'Senin', '12:50', '14:29'),
+        ('D', 'Rabu', '10:20', '11:59'),
+        ('E', 'Rabu', '07:00', '08:39'),
+        ('F', 'Selasa', '12:50', '14:29'),
+        ('G', 'Rabu', '10:20', '11:59'),
     ],
-    "Etika Profesi": [
-        "Senin 10:00 - 12:00", "Selasa 14:00 - 16:00",
-        "Rabu 08:00 - 10:00", "Kamis 10:00 - 12:00",
-        "Jumat 14:00 - 16:00", "Sabtu 14:00 - 16:00"
+    'Desain dan Analisis Algoritma': [
+        ('A', 'Jumat', '08:40', '10:19'),
+        ('B', 'Senin', '07:50', '09:29'),
+        ('C', 'Selasa', '13:40', '15:19'),
+        ('D', 'Jumat', '07:50', '09:29'),
+        ('E', 'Kamis', '15:30', '17:09'),
+        ('F', 'Selasa', '15:30', '17:09'),
+        ('G', 'Rabu', '07:00', '08:39'),
     ],
-    "Basis Data": [
-        "Senin 08:00 - 10:00", "Selasa 10:00 - 12:00",
-        "Rabu 14:00 - 16:00", "Kamis 08:00 - 10:00",
-        "Jumat 10:00 - 12:00", "Sabtu 08:00 - 10:00"
+    'Kecerdasan Buatan': [
+        ('A', 'Senin', '07:00', '08:39'),
+        ('B', 'Rabu', '09:30', '11:09'),
+        ('C', 'Kamis', '07:00', '08:39'),
+        ('D', 'Rabu', '13:40', '15:19'),
+        ('E', 'Rabu', '12:50', '14:29'),
+        ('F', 'Rabu', '12:50', '14:29'),
+        ('G', 'Kamis', '14:30', '16:19'),
     ],
-    "Jaringan Komputer": [
-        "Senin 14:00 - 16:00", "Selasa 08:00 - 10:00",
-        "Rabu 10:00 - 12:00", "Kamis 14:00 - 16:00",
-        "Jumat 08:00 - 10:00", "Sabtu 10:00 - 12:00"
+    'Metode Numerik': [
+        ('A', 'Rabu', '09:30', '11:09'),
+        ('B', 'Jumat', '07:00', '08:39'),
+        ('C', 'Senin', '14:30', '16:19'),
+        ('D', 'Kamis', '14:30', '16:19'),
+        ('E', 'Selasa', '07:00', '08:39'),
+        ('F', 'Kamis', '07:00', '08:39'),
+        ('G', 'Senin', '09:30', '11:09'),
     ],
-    "Metode Numerik": [
-        "Senin 08:00 - 10:00", "Selasa 10:00 - 12:00",
-        "Rabu 14:00 - 16:00", "Kamis 08:00 - 10:00",
-        "Jumat 10:00 - 12:00", "Sabtu 08:00 - 10:00"
-    ],
-    "Interaksi Manusia dan Komputer": [
-        "Senin 14:00 - 16:00", "Selasa 08:00 - 10:00",
-        "Rabu 10:00 - 12:00", "Kamis 14:00 - 16:00",
-        "Jumat 08:00 - 10:00", "Sabtu 10:00 - 12:00"
-    ],
-    "Desain dan Analisis Algoritma": [
-        "Senin 08:00 - 10:00", "Selasa 10:00 - 12:00",
-        "Rabu 14:00 - 16:00", "Kamis 08:00 - 10:00",
-        "Jumat 10:00 - 12:00", "Sabtu 08:00 - 10:00"
+     'Interaksi Manusia dan Komputer': [
+        ('A', 'Rabu', '08:30', '11:09'),
+        ('B', 'Jumat', '07:00', '08:39'),
+        ('C', 'Senin', '14:30', '16:19'),
+        ('D', 'Kamis', '14:30', '16:19'),
+        ('E', 'Selasa', '07:00', '08:39'),
+        ('F', 'Kamis', '07:00', '08:39'),
+        ('G', 'Senin', '09:30', '11:09'),
     ]
 }
 
-# Data SKS Mata Kuliah
-sks_data = {
-    "Pancasila": 2,
-    "Kecerdasan Buatan": 3,
-    "Etika Profesi": 2,
-    "Basis Data": 3,
-    "Jaringan Komputer": 2,
-    "Metode Numerik": 3,
-    "Interaksi Manusia dan Komputer": 3,
-    "Desain dan Analisis Algoritma": 3
-}
-
-# Jumlah Mahasiswa
-jumlah_mahasiswa = {
-    "Pancasila": 30,
-    "Kecerdasan Buatan": 40,
-    "Etika Profesi": 30,
-    "Basis Data": 40,
-    "Jaringan Komputer": 40,
-    "Metode Numerik": 40,
-    "Interaksi Manusia dan Komputer": 40,
-    "Desain dan Analisis Algoritma": 40
-}
-
-
-# Fungsi untuk menghitung nilai konflik
-def hitung_nilai_konflik(slot_waktu, matakuliah, jadwal):
-    # Implementasi rumus nilai konflik (sesuai dengan keterangan)
-    # Misalnya, kita asumsikan bahwa constraint ruangan dan waktu dosen tidak diberikan pada contoh ini.
-    n1 = 0.1  # Constraint ruangan (contoh: 0.1)
-    n2 = 0.45  # Constraint waktu dosen (contoh: 0.45)
-    n3 = 0.45 if len(jadwal[slot_waktu]) > 1 else 0  # Tipe kelas (contoh: 0.45 atau 0)
-    cd = 1  # Banyak constraint waktu/sks matakuliah (contoh: 1)
-
-    nilai_konflik = n1 + n2 + n3
-    return nilai_konflik
-
-# Fungsi untuk menghitung nilai kualitas slot waktu (H dalam A*)
-def hitung_kualitas_slot_waktu(slot_waktu, matakuliah, jadwal):
-    # Implementasi rumus nilai kualitas slot waktu (sesuai dengan keterangan)
-    # Misalnya, kita asumsikan bahwa P1, P2, dan P3 tidak diberikan pada contoh ini.
-    p1 = 0.4  # Contoh nilai P1
-    p2 = 0.35  # Contoh nilai P2
-    p3 = 0.25  # Contoh nilai P3
-
-    kualitas_slot_waktu = p1 + p2 + p3
-    return kualitas_slot_waktu
-
-# Fungsi A* Search untuk mencari slot waktu terbaik
-def a_star_search(matakuliah, sks, jumlah_mahasiswa):
-    jadwal = {}  # Dictionary untuk menyimpan jadwal
-    for slot in matakuliah_data[matakuliah]:
-        jadwal[slot] = []
-
-    # Fungsi untuk menghitung biaya total (F dalam A*)
-    def hitung_biaya_total(slot_waktu):
-        g = len(jadwal[slot_waktu]) * sks
-        h = hitung_kualitas_slot_waktu(slot_waktu, matakuliah, jadwal)
-        return g + h
-
-    # Heap priority queue untuk menyimpan slot waktu yang akan diekspansi
-    open_list = [(0, matakuliah_data[matakuliah][0])]  # (biaya, slot_waktu)
-    
-    while open_list:
-        biaya, current_slot = heapq.heappop(open_list)
-
-        # Jika slot waktu memiliki kapasitas cukup
-        if len(jadwal[current_slot]) + jumlah_mahasiswa <= 60:
-            jadwal[current_slot].append(matakuliah)
-
-            # Jika semua SKS terpenuhi, kembalikan jadwal
-            if sum(sks_data[m] for m in matakuliah_data) == sum(len(jadwal[slot]) * sks_data[m] for m in matakuliah_data):
-                return jadwal
-
-            # Ekspansi ke slot waktu berikutnya
-            for next_slot in matakuliah_data[matakuliah]:
-                heapq.heappush(open_list, (hitung_biaya_total(next_slot), next_slot))
-
-    # Jika tidak ditemukan jadwal yang memenuhi semua constraint
-    return None
-
-# Uji coba untuk semua mata kuliah
-for matakuliah_uji_coba in matakuliah_data.keys():
-    sks_uji_coba = sks_data[matakuliah_uji_coba]
-    jumlah_mahasiswa_uji_coba = jumlah_mahasiswa[matakuliah_uji_coba]
-
-    hasil_jadwal = a_star_search(matakuliah_uji_coba, sks_uji_coba, jumlah_mahasiswa_uji_coba)
-
-    # Tampilkan hasil jadwal
-    print(f"Jadwal untuk {matakuliah_uji_coba}:")
-    for slot, matkul in hasil_jadwal.items():
-        print(f"{slot}: {matkul}")
+# Cari dan cetak jadwal yang optimal
+optimal_schedule = find_schedule(classes)
+print_schedule(optimal_schedule)
